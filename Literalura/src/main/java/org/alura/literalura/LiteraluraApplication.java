2c6f082e6b34b8938193c0a1f1a80f212885cdfe
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-05 10:22:37 -0500

    Add initial config

---
+++ Literalura/src/main/java/org/alura/literalura/LiteraluraApplication.java
@@ -0,0 +1,18 @@
+package org.alura.literalura;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LiteraluraApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Hello, World!");
+  }
+}
