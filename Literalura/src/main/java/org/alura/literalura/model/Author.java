c5f4f6d58ac2b8a707c8103ffaee8f3e9e143fec
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 10:24:40 -0500

    Add the necessary classes

--- Literalura/src/main/java/org/alura/literalura/model/Author.java
+++ Literalura/src/main/java/org/alura/literalura/model/Author.java
@@ -1,2 +1,77 @@
-package org.alura.literalura.model;public class Author {
+package org.alura.literalura.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
+@Table(name = "authors")
+public class Author {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private int birthYear;
+  private int deathYear;
+  @OneToMany(mappedBy ="autor", fetch = FetchType.EAGER)
+  private List<Books> books;
+
+  public Author(String name, int deathYear, int birthYear) {
+    this.name = name;
+    this.deathYear = deathYear;
+    this.birthYear = birthYear;
+  }
+
+  public Author() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getBirthYear() {
+    return birthYear;
+  }
+
+  public void setBirthYear(int birthYear) {
+    this.birthYear = birthYear;
+  }
+
+  public int getDeathYear() {
+    return deathYear;
+  }
+
+  public void setDeathYear(int deathYear) {
+    this.deathYear = deathYear;
+  }
+
+  public List<Books> getBooks() {
+    return books;
+  }
+
+  public void setBooks(List<Books> books) {
+    books.forEach(book -> book.setAutor(this));
+    this.books = books;
+  }
+
+  @Override
+  public String toString() {
+    return "Author{" +
+            "name='" + name + '\'' +
+            ", birthYear=" + birthYear +
+            ", deathYear=" + deathYear +
+            '}';
+  }
 }
+
--- Literalura/src/main/java/org/alura/literalura/model/Books.java
+++ Literalura/src/main/java/org/alura/literalura/model/Books.java
@@ -1,14 +1,85 @@
 package org.alura.literalura.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class Libros {
+@Entity
+public class Books {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(unique = true)
   private String titulo;
   private int decargas;
-  private List<DataAuthors> autores;
-  private List<String> idiomas;
+  @ManyToOne(fetch = FetchType.EAGER)
+  private Author autor;
+  @ElementCollection(targetClass = Languages.class, fetch = FetchType.EAGER)
+  @CollectionTable(name = "lenguajes", joinColumns = @JoinColumn(name = "book_id"))
+  @Enumerated(EnumType.STRING)
+  @Column(name = "language")
+  private List<Languages> idiomas;
 
-  public Libros(String titulo , int descargas , DataAuthors dataAuthors)
+  public Books(DataBooks dataBooks , DataAuthors dataAuthors ){
+    this.titulo = dataBooks.titulo();
+    this.decargas = dataBooks.decargas();
+    this.autor = new Author(dataAuthors.nombre() , dataAuthors.anioMuerte() , dataAuthors.anioNacimiento());
+    this.idiomas = Languages.getLanguages(dataBooks.idiomas()).stream()
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());;
+  }
+
+  public Books(){
+
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public int getDecargas() {
+    return decargas;
+  }
+
+  public void setDecargas(int decargas) {
+    this.decargas = decargas;
+  }
+
+  public Author getAutor() {
+    return autor;
+  }
+
+  public void setAutor(Author autor) {
+    this.autor = autor;
+  }
+
+  public List<Languages> getIdiomas() {
+    return idiomas;
+  }
+
+  public void setIdiomas(List<Languages> idiomas) {
+    this.idiomas = idiomas;
+  }
+
+  @Override
+  public String toString() {
+    return "Titulo : " + titulo + "\n" +
+            "Autor : " + autor.getName() + "\n" +
+            "Idiomas : " + idiomas + "\n"+
+            "Número de decargas : " + decargas + "\n" ;
+  }
 }
