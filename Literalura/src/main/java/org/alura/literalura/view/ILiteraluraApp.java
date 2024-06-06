b5945fa4ac1c0bf2b7566af6108a75d0201e9c46
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 17:33:12 -0500

    Add the interface that will contain the methods that the App will use

--- Literalura/src/main/java/org/alura/literalura/view/ILiteraluraApp.java
+++ Literalura/src/main/java/org/alura/literalura/view/ILiteraluraApp.java
@@ -1,2 +1,9 @@
-package org.alura.literalura.view;public interface ILiteraluraApp {
+package org.alura.literalura.view;
+
+public interface ILiteraluraApp {
+  void showBooks();
+  void listBooksDataBase();
+  void listRegisterAuthorsDataBase();
+  void listAuthorsAliveInYear();
+  void listBooksByLanguage();
 }
