22a704095c80c72e618d0b7facc807e65ba8a6d5
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 17:28:44 -0500

    Add the class that will return the data

--- Literalura/src/main/java/org/alura/literalura/services/BookService.java
+++ Literalura/src/main/java/org/alura/literalura/services/BookService.java
@@ -1,2 +1,19 @@
-package org.alura.literalura.services;public class BookService {
+package org.alura.literalura.services;
+
+import org.alura.literalura.model.DataResultsBooks;
+
+public class BookService {
+  private final ConsumeAPI consumeAPI;
+  private final DataConversion dataConversion;
+
+  public BookService(ConsumeAPI consumeAPI, DataConversion dataConversion) {
+    this.consumeAPI = consumeAPI;
+    this.dataConversion = dataConversion;
+  }
+  // Metodo que consume la API de Gutendex y retorna los datos en un objeto DataResultsBooks
+  public DataResultsBooks getDataResults() {
+    String url = "https://gutendex.com/books/?page=1";
+    var json = consumeAPI.getData(url);
+    return dataConversion.convertData(json, DataResultsBooks.class);
+  }
 }
