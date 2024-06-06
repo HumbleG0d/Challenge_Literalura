9ef6f2c1beb6ecb9c0a9b3327f9b7723faeac062
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 17:34:40 -0500

    Add application class

--- Literalura/src/main/java/org/alura/literalura/LiteraluraApplication.java
+++ Literalura/src/main/java/org/alura/literalura/LiteraluraApplication.java
@@ -1,5 +1,9 @@
 package org.alura.literalura;
 
+import org.alura.literalura.repository.AuthorRepository;
+import org.alura.literalura.repository.BookRepository;
+import org.alura.literalura.view.LiteraluraApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
+  @Autowired
+  private BookRepository bookRepository;
+  @Autowired
+  private AuthorRepository authorRepository;
+
   public static void main(String[] args) {
     SpringApplication.run(LiteraluraApplication.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("Hello, World!");
+    LiteraluraApp literaluraApp = new LiteraluraApp(bookRepository , authorRepository);
+    literaluraApp.playApp();
   }
 }
