3b68ef2c53d02e77ae40b0971ca10a4aff1cda68
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 17:29:56 -0500

    Add class for author queries

--- Literalura/src/main/java/org/alura/literalura/repository/AuthorRepository.java
+++ Literalura/src/main/java/org/alura/literalura/repository/AuthorRepository.java
@@ -2,6 +2,15 @@ package org.alura.literalura.repository;
 
 import org.alura.literalura.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+// Interface que extiende de JpaRepository para realizar operaciones CRUD en la tabla de autores
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+  @Query("SELECT a FROM Author a WHERE a.birthYear <= :year AND a.deathYear >= :year")
+  List<Author> searchAuthorbyYear(int year);
+  Optional<Author> findByName(String name);
 }
