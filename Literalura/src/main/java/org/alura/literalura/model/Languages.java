73a215ee36114e6990d29afee0a75289a9d1849b
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 10:23:05 -0500

    Add an enum for languages

--- Literalura/src/main/java/org/alura/literalura/model/Languages.java
+++ Literalura/src/main/java/org/alura/literalura/model/Languages.java
@@ -1,4 +1,26 @@
 package org.alura.literalura.model;
 
-public enum Idiomas {
+import java.util.Arrays;
+import java.util.List;
+
+//Este enum contiene los idiomas en los que se pueden encontrar los libros
+public enum Languages {
+  ENGLISH("en"),
+  SPANISH("es");
+
+  private final String language;
+
+  Languages(String language){
+    this.language = language;
+  }
+
+  //Este metodo recibe una lista de idiomas y devuelve una lista de idiomas validos
+  public static List<Languages> getLanguages(List<String> languages){
+    return Arrays.stream(Languages.values()).map(l ->{
+      if(languages.contains(l.language)){
+        return l;
+      }
+      return null;
+    }).toList();
+  }
 }
