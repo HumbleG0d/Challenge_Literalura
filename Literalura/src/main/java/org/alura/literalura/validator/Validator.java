c000d8f198fd671f7fa317d29eab0052a00efd42
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 17:32:14 -0500

    Add the case that will use the interfaces to validate

--- Literalura/src/main/java/org/alura/literalura/validator/Validator.java
+++ Literalura/src/main/java/org/alura/literalura/validator/Validator.java
@@ -1,2 +1,49 @@
-package org.alura.literalura.validator;public class Validator {
+package org.alura.literalura.validator;
+
+import java.util.Scanner;
+
+public class Validator implements IValidator , IValidApp {
+  // Metodo que obtiene un string
+  @Override
+  public String getString(Scanner scanner, String message) {
+    System.out.print(message);
+    return scanner.nextLine();
+  }
+
+  // Metodo que valida un entero
+  @Override
+  public int validInteger(Scanner scanner, String message) {
+    int i = 0;
+    boolean isValid = false;
+    while(!isValid){
+      System.out.print(message);
+      if(scanner.hasNextInt()){
+        i = scanner.nextInt();
+        isValid = true;
+      }
+      else{
+        System.out.println("Este valor no es valido, intente de nuevo");
+      scanner.nextLine();
+      }
+    }
+    return i;
+  }
+
+  // Metodo que valida una opcion
+  @Override
+  public int validOption(Scanner scanner, String message, int min, int max) {
+    int i = 0;
+    boolean isValid = false;
+    while (!isValid){
+      i = validInteger(scanner, message);
+      if(i >= min && i <= max){
+        isValid = true;
+      }
+      else{
+        System.out.println("Esta opcion no es valida, intente de nuevo");
+      }
+      scanner.nextLine();
+    }
+    return i;
+  }
 }
