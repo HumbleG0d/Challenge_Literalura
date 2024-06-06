9dd7460e55bfef4e8499cd69f8b766f9c4401589
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-06-06 17:30:58 -0500

    Add interface valid

--- Literalura/src/main/java/org/alura/literalura/validator/IValidator.java
+++ Literalura/src/main/java/org/alura/literalura/validator/IValidator.java
@@ -1,2 +1,8 @@
-package org.alura.literalura.validator;public class IValidator {
+package org.alura.literalura.validator;
+
+import java.util.Scanner;
+
+public interface IValidator {
+  String getString(Scanner scanner, String message);
+  int validInteger(Scanner scanner, String message);
 }
